bd1bb60824cdce30b19eedec729d98fcb9e20550
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2012-05-04 10:29:32 +0300

    Now test server starts before tests and stops after all

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,28 @@ require 'aruba/cucumber'
 # See https://github.com/cucumber/aruba/blob/master/lib/aruba/api.rb for more information
 require 'aruba/api'
 
+# Making test project and start server on it
+require 'childprocess'
+FileUtils::mkdir_p('tmp/sandbox')
+server_process = nil
+Dir.chdir('tmp/sandbox') do
+  FileUtils::rm_rf('servertesting')
+  # Create new project. TODO: think what if tau projecter not works correctly
+  ChildProcess.build('tau new servertesting').start.wait
+
+  # Run server on test project directory
+  Dir.chdir('servertesting') do
+    server_process = ChildProcess.build('tau server')
+    server_process.start
+  end
+end
+
+SANDBOX_PROJECT_DIR = File.expand_path('tmp/sandbox/servertesting')
+
+at_exit do
+  server_process.stop
+end
+
 # Prepare test environment.
 # Build and intall our gem.
 # `tau something` command will be executed in test environment.
