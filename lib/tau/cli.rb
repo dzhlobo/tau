1746f33c784e38dcfc3385641203eba3acfbe418
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2012-05-14 11:47:46 +0300

    Version show added

--- lib/tau/cli.rb
+++ lib/tau/cli.rb
@@ -1,9 +1,11 @@
 require 'thor'
-require 'tau/server'
 require 'tau/projecter'
+require 'tau/server'
+require 'tau/version'
 
 module Tau
   class Cli < Thor
+    map '-v' => :version
 
     desc "new PROJECT_NAME", "Making new project"
     def new(project_name)
@@ -17,6 +19,11 @@ module Tau
     desc :server, "Starting server for development"
     def server
       Tau::Server.start
+    end
+
+    desc :version, "Show tau version"
+    def version
+      puts "Tau #{Tau::VERSION}"
     end
   end
 end
