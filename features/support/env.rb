3c41b64faeefc505838bb6ec6f62d628f2888085
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2012-10-04 18:52:44 +0300

    Prepare test environment before running server in sandbox

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,16 @@
-# Include useful steps. 
+# Include useful steps.
 # See full list at https://github.com/cucumber/aruba/blob/master/lib/aruba/cucumber.rb
 require 'aruba/cucumber'
 
 # Include aruba api for own steps
 # See https://github.com/cucumber/aruba/blob/master/lib/aruba/api.rb for more information
 require 'aruba/api'
+
+# Prepare test environment.
+# Build and intall our gem.
+# `tau something` command will be executed in test environment.
+require File.expand_path("#{File.dirname(__FILE__)}/cukegem.rb")
+CukeGem.setup('tau.gemspec')
 
 # Making test project and start server on it
 require 'childprocess'
@@ -27,9 +33,3 @@ SANDBOX_PROJECT_DIR = File.expand_path('tmp/sandbox/servertesting')
 at_exit do
   server_process.stop
 end
-
-# Prepare test environment.
-# Build and intall our gem.
-# `tau something` command will be executed in test environment.
-require File.expand_path("#{File.dirname(__FILE__)}/cukegem.rb")
-CukeGem.setup('tau.gemspec')
